b03d70ec43b3371bd0e01b0fbf365f09f8aa2bda
ming.huang <means789@gmail.com>
2024-06-21 01:36:35 +0100

    updated project config

--- x2_start.ps1
+++
@@ -1,5 +0,0 @@
-hatch clean
-hatch -h
-
-Write-Output ""
-Write-Output "Dependencies are synced when running a command e.g. hatch test"
--- x2_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-hatch clean
-hatch -h
-
-echo ""
-echo "Dependencies are synced when running a command e.g. hatch test"
