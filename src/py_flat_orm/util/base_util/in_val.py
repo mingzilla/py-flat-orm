5b1bdb5c99de7c6b59bdc2c32dcf38f3b30b436c
ming.huang <means789@gmail.com>
2024-06-22 22:12:42 +0100

    Updated case

--- src/py_flat_orm/util/base_util/in_val.py
+++ src/py_flat_orm/util/base_util/in_val.py
@@ -8,10 +8,10 @@ from datetime import date, time, datetime
 
 
 class InVal:
-    int_field = 0
-    float_field = 0.0
-    bool_field = False
-    str_field = ''
-    date_field = date(2000, 1, 1)
-    time_field = time(0, 0, 0)
-    datetime_field = datetime(2000, 1, 1, 0, 0, 1)
+    INT = 0
+    FLOAT = 0.0
+    BOOL = False
+    STR = ''
+    DATE = date(2000, 1, 1)
+    TIME = time(0, 0, 0)
+    DATE_TIME = datetime(2000, 1, 1, 0, 0, 1)
--- test_data/domain/all_common_types_obj.py
+++ test_data/domain/all_common_types_obj.py
@@ -4,10 +4,10 @@ from py_flat_orm.util.base_util.in_val import InVal
 class AllCommonTypesObj:
 
     def __init__(self):
-        self.int_field = InVal.int_field
-        self.float_field = InVal.float_field
-        self.bool_field = InVal.bool_field
-        self.str_field = InVal.str_field
-        self.date_field = InVal.date_field
-        self.time_field = InVal.time_field
-        self.datetime_field = InVal.datetime_field
+        self.int_field = InVal.INT
+        self.float_field = InVal.FLOAT
+        self.bool_field = InVal.BOOL
+        self.str_field = InVal.STR
+        self.date_field = InVal.DATE
+        self.time_field = InVal.TIME
+        self.datetime_field = InVal.DATE_TIME
